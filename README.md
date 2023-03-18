197c7f6a727ecd320cff388c1004dcf248022588
Mark Menge <markmenge@yahoo.com>
2023-03-18 18:30:06 -0400

    Renamed the command aicmd.json

--- README.md
+++ README.md
@@ -1,32 +1,25 @@
 # AI Command Line Interface
 
-This Python script, `ai.py`, allows you to access ChatGPT from the command line!
+This Python script, `aicmd.py`, allows you to access ChatGPT from the command line!
 
-Wouldn't you like to do this?
-python ai.py none "who was picasso?"
-
-python ai.py none "write python code to generate a 3D surface plot of z = sin(x) + cos(y)"
-
-Instructions
-1. Install python. Use method of choice, I'm running Windows 10 and I ran: winget install Python.Python.3.9
-2. Create an openai.com account. Be warned, you'll need a phone number. Don't worry it's backed by Microsoft.
-3. Generate an API key here: https://beta.openai.com/account/api-keys
-4. Open ai.json in notepad and edit "api_key": and set with your key.
-5. Run pip install openai
+Before using, set the "api_key" setting in ai.json. Currently you generate a key here: https://beta.openai.com/account/api-keys
 
 To run the script, use the following command:
-python ai.py [macro] "english description of what you want to do"
+
+python aicmd.py [macro] "english description of what you want to do"
 
 Example macros (stored in ai.json):
-- 'none' for no prompt string other than what you typed
 - 'bash' for Linux and macOS systems command prompts
 - 'cmd' for Windows command prompt
 - 'esp32' for ESP32 Arduino board code generation
 - 'python' for python code
-
+- 'none' for no prompt string other than what you typed
+- 
 Examples:
-python ai.py none "who was picasso?"
-python ai.py cmd "List files sorted by size"
-python ai.py bash "How much memory does this computer have"
-python ai.py python "create a webserver that says hello world, with a button that when you push it says hello to you!"
-python ai.py esp32 "create a webserver that says hello and the voltages on the pins. Create the access point named arduino with no password."  
+
+py aicmd.py none "who was picasso?"
+
+python aicmd.py cmd "List files sorted by size"
+python aicmd.py bash "How much memory does this computer have"
+python aicmd.py python "create a webserver that says hello world, with a button that when you push it says hello to you!"
+python aicmd.py esp32 "create a webserver that says hello and the voltages on the pins. Create the access point named arduino with no password."  
--- aicmd.py
+++ aicmd.py
@@ -6,7 +6,7 @@ import os
 
 def get_prompt(task):
     """Gets the prompt for the specified task from the config file."""
-    with open('ai.json', 'r') as f:
+    with open('aicmd.json', 'r') as f:
         config = json.load(f)
 
     return config['macros'][task]['cmd']
@@ -18,39 +18,41 @@ def execute_command(command):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='AI Command Line Tool', epilog='Example:python ai.py cmd "list all the .py files"')
+    parser = argparse.ArgumentParser(description='AI Command Line Tool', epilog='Example:python aicmd.py cmd "list all the .py files"')
     parser.add_argument('macro', type=str, help='The macro to perform (cmd, bash, esp32, python, none)')
     parser.add_argument('query', type=str, help='The natural language query')
     parser.add_argument('--no-exec', dest='no_exec', action='store_true',
                         help='Do not execute the command after generating it')
-    # parser.epilog('Example: python ai.py cmd list all the .py files')
+    # parser.epilog('Example: python aicmd.py cmd list all the .py files')
     args = parser.parse_args()
 
     # Configure the OpenAI API client
-    with open('ai.json', 'r') as f:
+    with open('aicmd.json', 'r') as f:
         config = json.load(f)
 
     openai.api_key = config['settings']['api_key']
     openai.api_base = config['settings']['api_endpoint']
 
     # Build the prompt
-    prompt = get_prompt(args.macro) + ' ' + args.query
-
+    prompt = get_prompt(args.macro) + args.query
+    prompt = prompt.strip()
+    if config['macros'][args.macro]['exec']:
+        prompt = prompt + " Shorter is better and offer no explanation."
     # Generate the command using OpenAI's GPT-3 API
     
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
-        max_tokens=128,
+        max_tokens=1024,
         n=1,
         stop=None,
-        temperature=0.25,
+        temperature=0.5,
     )
     output = response.choices[0].text.strip()
     
     # output = 'dir /s'
-    print(f"Openai.com was asked to:\n{prompt}") 
-    print(f"Openai.com responds with:\n{output}\n")
+    print(f"Openai.com was asked:\n{prompt}") 
+    print(f"Openai.com responded:\n{output}\n")
     
     
     # Execute the command if requested
